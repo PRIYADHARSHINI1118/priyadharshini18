6ef7eb80e1b4cf0a7fe9b50a0a2abf806c524063
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-02-15 09:53:33 +0530

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s="hello";
+	int n;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cout<<s<<endl;
+	}
+	return 0;
+}
