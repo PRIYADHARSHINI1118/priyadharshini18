bf0fe83a025a8f76e0d94653f466d3f0c4de2df6
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-02-28 10:15:57 +0530

    Update fact2.cpp

--- fact2.cpp
+++ fact2.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main() {
-	int num, i, fact=1;
+	int n, i, fact=1;
 	cout<<"Enter a number : ";
-	cin>>num;
-	for(i=num; i>0; i--)
+	cin>>n;
+	for(i=n; i>0; i--)
 	{
 		fact=fact*i;
 	}
-	cout<<"Factorial of "<<num<<" is "<<fact;
+	cout<<"Factorial of "<<n<<" is "<<fact;
 	return 0;
 }
