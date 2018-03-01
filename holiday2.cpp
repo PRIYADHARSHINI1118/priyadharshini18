a17ae2060a3116c4c4012cda0d35dd680307d83d
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-01 13:35:47 +0530

    Create holiday2.cpp

---
+++ holiday2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+string n;
+string day="sunday";
+cin>>n;
+if(n==day)
+{
+    cout<<"the day is holiday"<<endl;
+}
+else
+{
+    cout<<"the day is week days"<<endl;
+}
+    return 0;
+}
