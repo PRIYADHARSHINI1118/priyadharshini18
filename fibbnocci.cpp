169f93a0d838f97d09f966fa3709d2a34a1ab126
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-02 19:56:39 +0530

    Update fibbnocci.cpp

--- fibbnocci.cpp
+++ fibbnocci.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n, t1 = 0, t2 = 1, nextTerm = 0;
+    int n, t1 = 0, t2 = 1, nt = 0;
 
     cout << "Enter the number of terms: ";
     cin >> n;
@@ -12,7 +12,6 @@ int main()
 
     for (int i = 1; i <= n; ++i)
     {
-        // Prints the first two terms.
         if(i == 1)
         {
             cout << " " << t1;
@@ -23,11 +22,11 @@ int main()
             cout << t2 << " ";
             continue;
         }
-        nextTerm = t1 + t2;
+        nt= t1 + t2;
         t1 = t2;
-        t2 = nextTerm;
+        t2 = nt;
         
-        cout << nextTerm << " ";
+        cout << nt << " ";
     }
     return 0;
 }
