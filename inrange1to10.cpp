1df84bad8b48f248a33b93e7ddce17fe01733669
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-02 20:44:17 +0530

    Create inrange1to10.cpp

---
+++ inrange1to10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,i;
+    cin>>a;
+    if(a>=0&&a<=10)
+    {
+        cout<<a;
+    }
+    else
+    {
+        cout<<"not in range";
+    }
+    return 0;
+}
