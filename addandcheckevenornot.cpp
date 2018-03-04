6449c69e83bc397273093d3c78782ef392820eb4
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-04 19:46:33 +0530

    Create addandcheckevenornot.cpp

---
+++ addandcheckevenornot.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main( )
+{
+	int a,b,c;
+	cin>>a>>b;
+	c=a+b;
+	if((c%2)==0)
+	{
+	    cout<<"the number is even";
+	}
+else
+{
+    cout<<"the number is odd";
+}
+	return 0;
+}
