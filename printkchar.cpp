08e60584b67e70258f8e991508d1d4d9603a919c
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-04 19:24:35 +0530

    Create printkchar.cpp

---
+++ printkchar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int a,i;
+char s[20];
+cout<<"Enter the string"<<endl;
+cin>>s;
+cout<<"Enter the number"<<endl;
+cin>>a;
+for(i=0;i<a;i++)
+{
+cout<<s[i];
+}
+return 0;
+}
