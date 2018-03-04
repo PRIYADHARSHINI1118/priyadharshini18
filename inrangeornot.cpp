328cdbe25f32bd8fe297f0fc10ab5eba12b2cd0c
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-04 20:26:51 +0530

    Create inrangeornot.cpp

---
+++ inrangeornot.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,i,b,c;
+    cin>>a>>b>>c;
+    if(a>=b&&a<=c)
+    {
+        cout<<"this in range"<<a;
+    }
+    else
+    {
+        cout<<"not in range";
+    }
+    return 0;
+}
