679134abd7aab5fc6f5da3615a2295c54627ed7f
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-12 20:47:54 -0700

    Create 13multi.cpp

---
+++ 13multi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int b;
+  cout<<"Enter the number"<<endl;
+      cin>>b;
+      if(b%13==0)
+      {
+          cout<<"Yes";
+      }
+      else
+      {
+          cout<<"No";
+      }
+  return 0;
+    }
