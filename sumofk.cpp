b365412d35e6319e7be02d661a44d351e64bed9f
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-13 20:59:44 -0700

    Create sumofk.cpp

---
+++ sumofk.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,i,sum=0;
+    cin>>a;
+    if(a<=100)
+    {
+    for(i=1;i<=a;i++)
+{
+    sum=sum+i;
+    
+}
+cout<<sum;
+}
+    return 0;
+}
