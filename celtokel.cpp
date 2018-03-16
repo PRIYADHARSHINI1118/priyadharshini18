9fd656ca513eb7aa6d5ff0d7c17d460cd32f993b
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-15 21:22:08 -0700

    Create celtokel.cpp

---
+++ celtokel.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"the celsius value";
+    cin>>a;
+    b=a+273;
+    cout<<"kelvin value";
+    cout<<b;
+    return 0;
+}
