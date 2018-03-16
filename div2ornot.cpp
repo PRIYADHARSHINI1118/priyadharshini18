dcfebb2d92d38815329e241beea6490669c4e818
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-15 21:05:37 -0700

    Create div2ornot.cpp

---
+++ div2ornot.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a;
+      if(a%2==0)
+    {
+        b=a/2;
+        cout<<b;
+    }
+    else
+    {
+        cout<<a;
+    }
+    return 0;
+}
