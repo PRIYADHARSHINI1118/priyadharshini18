4590b2ee5dc31a7a048aa47620331dd8e9745ca4
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-15 21:12:26 -0700

    Create powofn.cpp

---
+++ powofn.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int n,k,a;
+    cout<<"enter the values";
+    cin>>n>>k;
+    a=pow(n,k);
+    cout<<"the power of n is";
+    cout<<a;
+    return 0;
+}
