73aa333c06cd6d9e51f28548858ddb97efa21c92
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-18 20:45:39 -0700

    Create cube.cpp

---
+++ cube.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int length,width,height,total;
+   cout<<"enter the length value:";
+   cin>>length;
+   cout<<"enter the width value:";
+   cin>>width;
+   cout<<"enter the height value:";
+   cin>>height;
+   total=length*width*height;
+   cout<<"the volume of cuboid:";
+   cout<<total;
+    return 0;
+}
