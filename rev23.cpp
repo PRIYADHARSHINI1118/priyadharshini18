f7a2cb918dcc6966a3e7e213464578cddfb41b99
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-18 21:18:50 -0700

    Create rev23.cpp

---
+++ rev23.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, rev=0, rem;
+	cout<<"Enter a number : ";
+	cin>>a;
+	while(a!=0)
+	{
+		rem=a%10;
+		rev=rev*10+rem;
+		a=a/10;
+	}
+	cout<<"Reverse = "<<rev;
+	return 0;
+}
