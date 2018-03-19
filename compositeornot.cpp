d439c8ec00988e77a0c2ea8199cefad164f1a50b
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-03-18 21:16:26 -0700

    Create compositeornot.cpp

---
+++ compositeornot.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int a,count=0;
+   cin>>a;
+  for(int i=1;i<=a;i++)
+{
+if (a%i==0)
+{
+count++;
+}	
+}
+if(count==2)
+{
+cout<<"this is not a composite number";
+}
+else
+{
+cout<<a<<" "<<"this is composite number";
+}
+   return 0;
+}
