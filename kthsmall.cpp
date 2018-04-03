758afba83301e373f73a5e9e30d47f1669943f3a
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-04-03 03:41:19 -0700

    Create kthsmall.cpp

---
+++ kthsmall.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,k,a[30];
+    cout<<"enter the n and k value"<<endl;
+    cin>>n>>k;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+        
+        {
+            cout<<a[k-1];
+        }
+        
+    return 0;
+}
