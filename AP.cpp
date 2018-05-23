e9edbbf6acebc19dccdd5426f8b3c68614f58792
PRIYADHARSHINI1118 <36472030+PRIYADHARSHINI1118@users.noreply.github.com>
2018-05-23 12:30:52 +0530

    Create AP.cpp

---
+++ AP.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include<math.h>
+int main()
+{
+ int a,d,n,i,nth;
+ int sum=0;
+ cout<<"\nEnter the first number of the series : ";
+ cin>>a;
+ cout<<"\nEnter the number of terms in series : ";
+ cin>>n;
+ cout<<"\nEnter the common difference of series : ";
+ cin>>d;
+ sum=(n*(2*a+(n-1)*d))/2;
+ nth=a+(n-1)*d;
+ cout<<"\nSum of the A.P. is : ";
+ for(i=a;i<=nth;i=i+d)
+ {
+  if(i!=nth)
+  {
+    cout<<i<<" + ";
+  }
+  else
+  {
+    cout<<i<<" = "<<sum;
+  }
+ }
+return 0;
+}
